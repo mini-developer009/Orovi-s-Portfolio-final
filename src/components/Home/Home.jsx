ee0568fb3108d72fc75eb048f6ae0e3510e667b1
Ashikul Islam <ashikonik999@gmail.com>
2025-11-02 11:44:35 +0600

    Update Home component layout with new sections
    Refactor Home component to include services, skills, and resume sections.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-import About from '../../pages/AboutMe/About';
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
+  return (
+    <div>
+      <div id="home">
+        <Banner></Banner>
+      </div>
+      <div id="services" className='pt-20'>
+        <h2 className='text-3xl font-bold'>Services Section</h2>
+      </div>
 
-           <div id="about">
-             <About></About>
-           </div>
+      <div id="skills" className='pt-20'>
+        <h2 className='text-3xl font-bold'>Skills Section</h2>
+      </div>
 
-        </div>
-    );
+      <div id="resume" className='pt-20'>
+        <h2 className='text-3xl font-bold'>Resume Section</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

dea027489f270abbaa638072099fe8d87d5a1d95
Ashikul Islam <ashikonik999@gmail.com>
2025-11-02 11:56:07 +0600

    Refactor imports and update router configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { 
+  createBrowserRouter,
+  RouterProvider 
+} from "react-router-dom"; 
+import './index.css';
 import Root from './components/Root/Root.jsx';
 import Home from './components/Home/Home.jsx';
 import About from './pages/AboutMe/About.jsx';
 import Project from './pages/projects/Project.jsx';
-
-
 
 const router = createBrowserRouter([
   {
@@ -20,21 +19,20 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
-        {
-          path: "/about",
-          element: <About></About>,
-        },
-       {
-    path: "/new",
-    element: <Project></Project>
-  }
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/new",
+        element: <Project></Project>
+      }
     ]
   },
- 
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
